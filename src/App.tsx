31e3fd9ef390c715b2232a060f7965aa30929932
gohyenther <goh.yenther@gmail.com>
2023-02-09 18:43:27 +0800

    read pdf file locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React, { useRef, useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 import WebViewer from '@pdftron/webviewer';
 import './App.css';
-import { parseJsonText } from 'typescript';
 
 
 function App() {
   const viewer = useRef<HTMLDivElement>(null);
 
-  // if using a class, equivalent of componentDidMount 
+  // if using a class, equivalent of componentDidMount
   useEffect(() => {
     WebViewer(
       {
         path: 'lib',
-        initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf',
+        initialDoc: 'lib/webviewer-demo.pdf'
+        // pdf downloaded from: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf'
       },
       viewer.current as HTMLDivElement).then((instance) => {
       const { documentViewer, annotationManager, Annotations } = instance.Core;
@@ -25,13 +25,16 @@ function App() {
         header.push({
           type: "actionButton",
           img: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
+          
           onClick: async () => {
             const export_xfdf = await annotationManager.exportAnnotations({links:false,widgets:false});
             console.log(export_xfdf);
+
             fetch('http://localhost:5000/annotation/add/' + studentId, {
               method: 'POST',
               body: export_xfdf // written to a database in the server
             });
+            
           }
         });
       });
